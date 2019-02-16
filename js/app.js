4d163cc7d5f07fb5a890e543bec4500effcd62c6
croschel <croschel000@gmail.com>
2019-02-16 15:06:53 -0200

    Now I can pay the game

--- js/app.js
+++ js/app.js
@@ -2,12 +2,11 @@
 
 //https://www.youtube.com/watch?time_continue=1&amp=&v=oLSu3zc2jSA // assistir depois
 
-var Enemy = function() {
+var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -15,8 +14,18 @@ var Enemy = function() {
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
+    this.x += this.speed * dt; // the moviment for enemies
+    console.log(this.x);
     // which will ensure the game runs at the same speed for
     // all computers.
+    if(this.x > 510){
+        this.x = -50;
+        this.speed = 100 + Math.floor(Math.random() * 222); // ramdomize the enemies
+    }
+    if(player.x < this.x + 80 && player.x + 80 > this.x && player.y < this.y + 60 && 60 + player.y > this.y){
+        player.x = 202; // reset the position of player to the beginning
+        player.y = 405;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +33,10 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-var Player = function() {
+var Player = function(x,y) {
     // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/char-boy.png';
 };
 
@@ -46,22 +53,42 @@ Player.prototype.update = function(dt) {
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-// Now write your own player class
 
-// This class requires an update(), render() and
-// a handleInput() method.
-
+//movement for Player with keyboard
+Player.prototype.handleInput = function(keyPress){ //I can check if the sprite arrive in the limit of canvas
+    if(keyPress == 'left' && this.x > 0){
+        this.x -=102;
+    }
+    if(keyPress == 'right' && this.x < 405){
+        this.x +=102;
+    }
+    if(keyPress == 'up' && this.y > 0){
+        this.y -=83;
+    }
+    if(keyPress == 'down' && this.y < 405){
+        this.y +=83;
+    }
+    if(this.y < 0){ //if I arrive in water so... I have to start the game again
+        setTimeout(function(){
+            player.x = 202;
+            player.y = 405;
+        },600);
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+const allEnemies = [];
+var enemyLoc = [63,147,230];//initial position for enemies
 
-
+enemyLoc.forEach(function(yLoc){
+    enemy = new Enemy(0,yLoc,200);
+    allEnemies.push(enemy);
+})
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this
-const player = new Player();
-const allEnemies = [];
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -73,3 +100,4 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
+const player = new Player(202,405);//object player
